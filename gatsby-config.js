88cfa5402d5e3feeabe9ad11851089d95fb30c42
tomato990 <495999432@qq.com>
2023-12-28 12:05:51 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
       resolve: `gatsby-plugin-gdpr-cookies`,
       options: {
         facebookPixel: {
-          pixelId: "375240921666166",
+          pixelId: "910346737139389",
           cookieName: "cookie-consent",
         },
         environments: ["production", "development"],
