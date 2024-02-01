7d0d510b6f72796e3c6108a931fbe29890d518be
tomato990 <495999432@qq.com>
2024-02-01 14:45:30 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,6 +14,22 @@ export const Header: React.FC = () => {
       <Link to="/">
         <h1 style={headingStyles}>Altmeta.org Pixel Test</h1>
       </Link>
+      <script>
+    var __cbSdkHost = "https://dlz4ju33qov8p.cloudfront.net/";
+    var __cbSdkSettings={'cbq':{destinations:[{name:"cloudbridge"},],
+    "host":__cbSdkHost,"globalVar":"cbq",}};
+    !function(a,h,e,v,n,t,s)
+      {if(a.cbq)return;n=a.cbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!a._cbq)a._cbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=h.createElement(e);t.async=!0;
+      t.src=v;s=h.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      __cbSdkHost+'sdk/capig-events.js');
+    cbq('init', "4790815198314486511");
+    cbq('loadConfig', "4790815198314486511");
+    cbq('track', 'PageView');
+    </script>
     </header>
   );
 };
